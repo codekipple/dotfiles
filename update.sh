2cb50af8af3515b5bd86975217a8ee1c406c49c1
codekipple <codekipple@gmail.com>
2017-12-18 15:58:28 +0000

    Removed vm dotfiles

--- update.sh
+++ update.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-if [ $1 == 'vm' ]; then
-    rsync -v --exclude-from 'exclude.txt' vm/.* ~
-fi
-
 rsync -v --exclude-from 'exclude.txt' .* ~
--- vm/.bashrc
+++
@@ -1,2 +0,0 @@
-# update time on login to make sure vm's time stays up to date
-sudo ntpdate pool.ntp.org
